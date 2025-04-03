57fda24bc0e827055d2c159d800cf761371b4cb3
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-04-02 20:26:40 -0400

    saving

--- code.py
+++ code.py
@@ -17,7 +17,8 @@ seconds_to_microseconds_conversion_number = 1000000
 sonar_delays = [2 / seconds_to_microseconds_conversion_number, 10 / seconds_to_microseconds_conversion_number]
 delay_between_sonar_cheeks = 10
 distance = 0
-TOO_CLOSE = 20
+servo_delay = 0.05
+TOO_CLOSE = 50
 
 # setup
 sonar = adafruit_hcsr04.HCSR04(trigger_pin = board.GP15, echo_pin = board.GP14)
@@ -41,11 +42,11 @@ while True:
     if distance < TOO_CLOSE:
         for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
             my_servo.angle = angle
-            time.sleep(blink_delay)
+            time.sleep(servo_delay)
     else:
         for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
             my_servo.angle = angle
-            time.sleep(blink_delay)
+            time.sleep(servo_delay)
 
     # The commented out code is not part of the actual code but is needed to get it working by uncommenting it and then recommenting it
     time.sleep(delay_between_sonar_cheeks)
